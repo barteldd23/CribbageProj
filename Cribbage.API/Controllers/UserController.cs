759eb133f9618a3fe4623a1dae00ae809dd3ade5
Dean Bartel <300089145@fvtc.edu>
2024-03-18 12:51:51 -0500

    Started API Controllers

---
+++ Cribbage.API/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Cribbage.PL.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cribbage.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+
+        private readonly ILogger<UserController> logger;
+        private readonly DbContextOptions<CribbageEntities> options;
+
+        public UserController( ILogger<UserController> logger, DbContextOptions<CribbageEntities> options)
+        {
+            this.logger = logger;
+            this.options = options;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
