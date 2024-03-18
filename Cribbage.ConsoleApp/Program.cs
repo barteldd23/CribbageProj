e7f59bfa2093f6429ec0c8d502cf60ac7b0fca51
Rachel Groff <rachelgroff176@gmail.com>
2024-03-18 17:00:56 -0500

    added SignalRConnection and updated ConsoleApp

--- Cribbage.ConsoleApp/Program.cs
+++ Cribbage.ConsoleApp/Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Cribbage.ConsoleApp;
+
+internal class Program
+{
+    private static string DrawMenu()
+    {
+        Console.WriteLine("Which operation do you wish to perform?");
+        Console.WriteLine("Connect to a channel (c)");
+        Console.WriteLine("Get the secret (g)");
+        Console.WriteLine("Send a message to the channel (s)");
+        Console.WriteLine("Exit (x)");
+
+        string operation = Console.ReadLine();
+        return operation;
+    }
+
+    private async static void GetSecret()
+    {
+
+        const string secretName = "Cribbage";
+        var keyVaultName = "kv-31590";
+        var kvUri = $"https://{keyVaultName}.vault.azure.net";
+
+        var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+        var secretValue = "SECRET_VALUE";
+
+        Console.Write($"Creating a secret in {keyVaultName} called '{secretName}' with the value '{secretValue}' ...");
+        await client.SetSecretAsync(secretName, secretValue);
+        Console.WriteLine(" done.");
+
+        Console.WriteLine("Forgetting your secret.");
+        secretValue = string.Empty;
+        Console.WriteLine($"Your secret is '{secretValue}'.");
+
+        Console.WriteLine($"Retrieving your secret from {keyVaultName}.");
+        var secret = await client.GetSecretAsync(secretName);
+        Console.WriteLine($"Your secret is '{secret.Value.Value}'.");
+
+        Console.Write($"Deleting your secret from {keyVaultName} ...");
+        //DeleteSecretOperation operation = await client.StartDeleteSecretAsync(secretName);
+        // You only need to wait for completion if you want to purge or recover the secret.
+        //await operation.WaitForCompletionAsync();
+        //Console.WriteLine(" done.");
+
+        //Console.Write($"Purging your secret from {keyVaultName} ...");
+        //await client.PurgeDeletedSecretAsync(secretName);
+        //Console.WriteLine(" done.");
+    }
+
+    private static void Main(string[] args)
+    {
+        string user = "CribbageUser";
+        //string hubAddress = "https://fvtcdp.azurewebsites.net/GameHub";
+        string hubAddress = "https://localhost:7186/CribbageHub";
+
+        string operation = DrawMenu();
+
+        var signalRConnection = new SignalRConnection(hubAddress);
+
+        while (operation != "x")
+        {
+            switch (operation)
+            {
+                case "c":
+                    signalRConnection.ConnectToChannel(user);
+                    break;
+                case "s":
+                    break;
+                case "x":
+                    break;
+            }
+
+            operation = DrawMenu();
+        }
+    }
+}
---
+++ Cribbage.ConsoleApp/SignalRConnection.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cribbage.ConsoleApp
+{
+    internal class SignalRConnection
+    {
+        private string hubAddress;
+        HubConnection _connection;
+        string user;
+
+        public SignalRConnection(string hubAddress)
+        {
+            this.hubAddress = hubAddress;
+        }
+
+        public void Start()
+        {
+            _connection = new HubConnectionBuilder()
+                .Build();
+                //.WithUrl(hubAddress) 
+                //.Build();
+
+            _connection.On<string, string>("ReceiveMessage", (s1, s2) => OnSend(s1, s2));
+
+            _connection.StartAsync();
+        }
+
+        private void OnSend(string user, string message)
+        {
+            Console.WriteLine(user + ": " + message);
+        }
+
+        public void ConnectToChannel(string user)
+        {
+            Start();
+            string message = user + " Connected";
+            try
+            {
+                _connection.InvokeAsync("SendMessage", "System", message);
+                //_connection.SendMessage("System", message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
