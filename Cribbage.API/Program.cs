e13bcf697fcb07eb2fa8fc56e60bf107c51ab36e
Rachel Groff <rachelgroff176@gmail.com>
2024-03-20 12:48:20 -0500

    fixed SignalR issues

--- Cribbage.API/Program.cs
+++ Cribbage.API/Program.cs
@@ -1,69 +1,74 @@
-using Cribbage.API.Hubs;
-
-using Cribbage.PL.Data;
 using Microsoft.EntityFrameworkCore;
+using Cribbage.API.Hubs;
+using Cribbage.PL.Data;
 using System.Reflection;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddSignalR()
-    .AddJsonProtocol(options =>
-    {
-        options.PayloadSerializerOptions.PropertyNamingPolicy = null;
-    });
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+public class Program
 {
-    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    private static void Main(string[] args)
     {
-        Title = "Cribbage API",
-        Version = "v1",
-        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddSignalR()
+            .AddJsonProtocol(options =>
+            {
+                options.PayloadSerializerOptions.PropertyNamingPolicy = null;
+            });
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
         {
-            Name = "Cribbage"
-            //Email = "rachel.groff2190@fvtc.edu",
-            //Url = new Uri("https://www.fvtc.edu")
+            c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+            {
+                Title = "Cribbage API",
+                Version = "v1",
+                Contact = new Microsoft.OpenApi.Models.OpenApiContact
+                {
+                    Name = "Cribbage"
+                    //Email = "rachel.groff2190@fvtc.edu",
+                    //Url = new Uri("https://www.fvtc.edu")
+                }
+            });
+
+            var xmlfile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlpath = Path.Combine(AppContext.BaseDirectory, xmlfile);
+            c.IncludeXmlComments(xmlpath);
+        });
+
+
+        // Add Connection information
+        builder.Services.AddDbContextPool<CribbageEntities>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection"));
+            options.UseLazyLoadingProxies();
+        });
+
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
-    });
 
-    var xmlfile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlpath = Path.Combine(AppContext.BaseDirectory, xmlfile);
-    c.IncludeXmlComments(xmlpath);
-});
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
 
+        //app.MapControllers();
 
-// Add Connection information
-builder.Services.AddDbContextPool<CribbageEntities>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection"));
-    options.UseLazyLoadingProxies();
-});
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapHub<CribbageHub>("/cribbageHub");
+        });
 
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseAuthorization();
-
-//app.MapControllers();
-
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-    endpoints.MapHub<CribbageHub>("/cribbageHub");
-});
-
-app.Run();
