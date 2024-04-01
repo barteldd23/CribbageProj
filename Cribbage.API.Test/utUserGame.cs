5750fc9cf1fb715ac45579fd23b005217b247bd9
Rachel Groff <rachelgroff176@gmail.com>
2024-04-01 13:56:36 -0500

    fixed API Test utUserGame

--- Cribbage.API.Test/utUserGame.cs
+++ Cribbage.API.Test/utUserGame.cs
@@ -28,24 +28,21 @@ namespace Cribbage.API.Test
         [TestMethod]
         public async Task LoadByIdTestAsync()
         {
-            //UserGame userGame = new UserGameManager(options).Load().FirstOrDefault();
-            UserGame userGame = new UserGame { Id = Guid.NewGuid(), GameId = Guid.NewGuid(), PlayerId = Guid.NewGuid(), PlayerScore = 0 };
-            await base.LoadByIdTestAsync<UserGame>(new KeyValuePair<string, string>("Id", userGame.Id.ToString()));
+            await base.LoadByIdTestAsync<UserGame>(new KeyValuePair<string, string>("Id", "554f727e-0a21-4236-9092-014dc1bd5ebb"));
         }
 
         [TestMethod]
         public async Task UpdateTestAsync()
         {
-            UserGame userGame = new UserGame { Id = Guid.NewGuid(), GameId = Guid.NewGuid(), PlayerId = Guid.NewGuid(), PlayerScore = 0 };
-            await base.UpdateTestAsync<UserGame>(new KeyValuePair<string, string>("Id", userGame.Id.ToString()), userGame);
+            UserGame userGame = new UserGame { GameId = new Guid("20112213-9177-4a42-9543-15a22b66ba26"), PlayerId = new Guid("a988fc08-9f27-46d0-a4fa-89769251fde3"), PlayerScore = 0 };
+            await base.UpdateTestAsync<UserGame>(new KeyValuePair<string, string>("Id", "a327a126-4fb0-4326-a70c-e527082cf2c3"), userGame);
 
         }
 
         [TestMethod]
         public async Task DeleteTestAsync()
         {
-
-            await base.DeleteTestAsync1<UserGame>(new KeyValuePair<string, string>("Id", 100.ToString()));
+            await base.DeleteTestAsync1<UserGame>(new KeyValuePair<string, string>("Id", "b20d5802-93c1-4560-946e-85678b076f01"));
         }
 
 
