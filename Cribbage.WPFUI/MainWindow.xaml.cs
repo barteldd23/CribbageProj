832c4223f440b576e6ab998ca53537ee34cc6f85
Rachel Groff <300062190@fvtc.edu>
2024-05-07 16:18:53 -0500

    fixed message to users when someone leaves a game WPF

--- Cribbage.WPFUI/MainWindow.xaml.cs
+++ Cribbage.WPFUI/MainWindow.xaml.cs
@@ -548,6 +548,8 @@ namespace Cribbage.WPFUI
 
         private void QuitGameMessage(string message)
         {
+            signalRMessage = message;
+
             if (mainMenuClick)
             {
                 StaThreadWrapper(() =>
@@ -561,6 +563,16 @@ namespace Cribbage.WPFUI
             else if (exitClick)
             {
                 Dispatcher.Invoke(() => { this.Close(); });
+            }
+            else
+            {
+                Dispatcher.Invoke(() => 
+                {
+                    lblMessageToPlayers.Content = signalRMessage;
+                    lstMessages.Items.Add(signalRMessage);
+                    lstMessages.SelectedIndex = lstMessages.Items.Count - 1;
+                    lstMessages.ScrollIntoView(lstMessages.SelectedItem);
+                });
             }
         }
 
